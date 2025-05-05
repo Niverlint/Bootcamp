53ff5a27d0ec9480f0b65454dc30f49ff4ecb6de
Amal <asanbekovamal@gmail.com>
2025-05-06 00:49:30 +0600

    задание 2

---
+++ target/debug/deps/task1.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\task1\target\debug\deps\task1.exe: src\main.rs
+
+C:\Users\Admin\task1\target\debug\deps\task1.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/task1.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\task1\target\debug\task1.exe: C:\Users\Admin\task1\src\main.rs
